b2809eb44bded516ee1c620a2668edc3d8689212
ssu00 <rachel4535@naver.com>
2022-01-12 22:25:55 +0900

    💄 edited style file (css->stylesheet)

---
+++ src/assets/fonts/typography.js
@@ -0,0 +1,52 @@
+import {StyleSheet} from 'react-native';
+const typoStyles = StyleSheet.create({
+  /* font-size */
+  fs32: {
+    fontSize: 32,
+  },
+  fs20: {
+    fontSize: 20,
+  },
+  fs18: {
+    fontSize: 18,
+  },
+  fs15: {
+    fontSize: 15,
+  },
+  fs14: {
+    fontSize: 14,
+  },
+  fs13: {
+    fontSize: 13,
+  },
+  fs12: {
+    fontSize: 12,
+  },
+
+  /* color */
+  textMain: {
+    color: '#19b7cd',
+  },
+  textPrimary: {
+    color: '#fb7348',
+  },
+  textDisable: {
+    color: '#dad8e0',
+  },
+  textExplain: {
+    color: '#737373',
+  },
+  textWhite: {
+    color: '#fff',
+  },
+
+  /* font-weight */
+  fwRegular: {
+    fontWeight: '400',
+  },
+  fwBold: {
+    fontWeight: '700',
+  },
+});
+
+export default typoStyles;
