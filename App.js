cd9adecebda2fa86babf7e05a399586d3abf57c4
zoowb <zoowb0829@gmail.com>
2022-01-19 01:24:34 +0900

    :lipstick: update login

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import StartMainScreen from './src/screens/StartMainScreen';
-import StartFirstScreen from './src/screens/StartFirstScreen';
-import StartSecondScreen from './src/screens/StartSecondScreen';
-import StartThirdScreen from './src/screens/StartThirdScreen';
+import StartMainScreen from './src/screens/start/StartMainScreen';
+import StartFirstScreen from './src/screens/start/StartFirstScreen';
+import StartSecondScreen from './src/screens/start/StartSecondScreen';
+import StartThirdScreen from './src/screens/start/StartThirdScreen';
+import LoginMainScreen from './src/screens/login/LoginMainScreen';
+import LoginScreen from './src/screens/login/LoginScreen';
+import SignUpScreen from './src/screens/signup/SignUpScreen';
+import SignUpDoneScreen from './src/screens/signup/SignUpDoneScreen';
+import SignUpDetailScreen from './src/screens/signup/SignUpDetailScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
@@ -11,7 +16,7 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="StartThird">
+      <Stack.Navigator initialRouteName="SignUpDone">
         <Stack.Screen
           options={{headerShown: false}}
           name="Start"
@@ -31,6 +36,31 @@ const App = () => {
           options={{headerShown: false}}
           name="StartThird"
           component={StartThirdScreen}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="LoginMain"
+          component={LoginMainScreen}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="SignUp"
+          component={SignUpScreen}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="SignUpDetail"
+          component={SignUpDetailScreen}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="SignUpDone"
+          component={SignUpDoneScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
