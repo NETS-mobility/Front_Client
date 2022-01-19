92b4054788a0892c54d58c8246c5e439bc18c9b9
zoowb <zoowb0829@gmail.com>
2022-01-19 13:44:16 +0900

    :heavy_plus_sign: change app.js

--- App.js
+++ App.js
@@ -8,14 +8,12 @@ import {
 import StartNavigator from './src/navigation/start/start';
 import LoginNavigator from './src/navigation/login/login';
 import SignUpNavigator from './src/navigation/signup/signup';
-import ReservationNavigator from './src/navigation/reservation/reservation';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="ReservationNavigator" component={ReservationNavigator} />
         <Stack.Screen name="StartNavigator" component={StartNavigator} />
         <Stack.Screen name="LoginNavigator" component={LoginNavigator} />
         <Stack.Screen name="SignUpNavigator" component={SignUpNavigator} />
