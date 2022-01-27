0d8030d5988d1e88421775e2561756269bac5034
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-01-27 22:09:54 +0900

    Add Server-ServiceLookup

--- servers/routes/service.js
+++ servers/routes/service.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const jwt = require('../modules/jwt');
 const pool = require('../modules/mysql');
 const pool2 = require('../modules/mysql2');
 
