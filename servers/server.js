d513b86d46add7c597c53dff3bb03e7698dd1a78
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-01-27 21:51:42 +0900

    Add Server-ServiceLookup

--- servers/server.js
+++ servers/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 // 라우팅 js파일 목록
 const route_service = require('./routes/service');
-const route_mypage = require('./routes/mypage');
 
 // npm 모듈 목록
 const cors = require("cors");
@@ -18,4 +17,3 @@ app.listen(port, () => console.log(`${port}`));
 
 // 서버별 도메인 설정 (app.use(bodyParser.json())코드 이후에 추가해야 bodyParser가 적용됨)
 app.use('/service', route_service);
-app.use('/mypage', route_mypage);
