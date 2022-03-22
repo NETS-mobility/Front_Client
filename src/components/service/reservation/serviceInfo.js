2feb7ab1a0a4aecd4dee7069e3d51fb0cff8bd98
ssu00 <rachel4535@naver.com>
2022-03-22 22:55:48 +0900

    ⚡️ complete service list & detail

--- src/components/service/reservation/serviceInfo.js
+++ src/components/service/reservation/serviceInfo.js
@@ -117,19 +117,19 @@ const pickCategory = (data, dispatch, pick) => {
   const categories = {
     1: ['서비스 번호', data?.service_id],
     2: ['픽업 주소', data?.pickup_address],
-    3: ['병원 주소', data?.drop_address],
+    3: ['병원 주소', data?.hos_address],
     4: ['귀가 주소', data?.drop_address],
-    5: ['픽업 예정 시간', data?.pickup_time.substring(0, 5)],
-    6: ['희망 병원 도착 시간', data?.hos_arrival_time.substring(0, 5)],
-    7: ['희망 귀가 출발 시간', data?.hos_arrival_time.substring(0, 5)],
+    5: ['픽업 예정 시간', data?.pickup_time?.substring(0, 5)],
+    6: ['희망 병원 도착 시간', data?.hos_arrival_time?.substring(0, 5)],
+    7: ['희망 귀가 출발 시간', data?.hos_depart_time?.substring(0, 5)],
     8: ['진료/검사 예약 시간', data?.hos_care_time.substring(0, 5)],
     9: ['차량 번호', dispatch?.[0]?.car_number],
     10: ['네츠 매니저', dispatch?.[0]?.netsmanager_name],
     11: ['동행 매니저', data?.gowithumanager],
     12: ['차량 번호 1', dispatch?.[0]?.car_number],
-    13: ['차량 번호 2', dispatch?.[0]?.car_number],
+    13: ['차량 번호 2', dispatch?.[1]?.car_number],
     14: ['네츠 매니저 1', dispatch?.[0]?.netsmanager_name],
-    15: ['네츠 매니저 2', dispatch?.[0]?.netsmanager_name],
+    15: ['네츠 매니저 2', dispatch?.[1]?.netsmanager_name],
   };
   const categoryKey = Object.keys(categories);
   for (let i = 0; i < categoryKey.length; i++) {
@@ -146,7 +146,7 @@ const CaseInfo = dispatchCase => {
     case 2: //병원-집
       return [1, 3, 4, 7, 9, 10, 11];
     case 3: //집-집 (배차 1번, 2시간 이하)
-      return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     case 4: //왕복2시간이상
       return [1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15];
   }
@@ -159,8 +159,7 @@ export const ServiceInfo = ({num, data, dispatch}) => {
     },
   });
 
-  let dispatchCase = 4; //============================ 여기 바꿔야 함 ============================
-  const infos = CaseInfo(dispatchCase);
+  const infos = CaseInfo(data?.dispatch_case);
 
   return (
     <View>
@@ -178,10 +177,11 @@ export const ServiceInfo = ({num, data, dispatch}) => {
         </Text>
       )}
 
-      {infos.map((infoData, i) => {
-        const result = pickCategory(data, dispatch, infoData);
-        return <ServiceInfoOneLine title={result[0]} value={result[1]} />;
-      })}
+      {infos != undefined &&
+        infos.map((infoData, i) => {
+          const result = pickCategory(data, dispatch, infoData);
+          return <ServiceInfoOneLine title={result[0]} value={result[1]} />;
+        })}
     </View>
   );
 };
