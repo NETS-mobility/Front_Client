c24affb873c577583951da5439332608f1b869a9
ssu00 <rachel4535@naver.com>
2022-04-12 07:46:07 +0900

    🐛 fix bug caused by native-base

--- src/components/pay_test/loading.js
+++ src/components/pay_test/loading.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import {Text, View} from 'native-base';
+import {HStack, Spinner, Heading, NativeBaseProvider} from 'native-base';
+import {View, StyleSheet} from 'react-native';
 
-const Loading = () => {
+const CommonLoading = () => {
+  const styles = StyleSheet.create({
+    fullPage: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+
   return (
-    <View
-      flex={1}
-      alignItems={'center'}
-      flexDir={'row'}
-      justifyContent={'center'}>
-      <View flex={1} alignItems={'center'} justifyContent={'center'}>
-        <Text fontSize={20}>잠시만 기다려주세요...</Text>
+    <NativeBaseProvider>
+      <View style={styles.fullPage}>
+        <HStack
+          space={5}
+          justifyContent="center"
+          alignItems="center"
+          alignSelf="center">
+          <Spinner accessibilityLabel="Loading posts" size="lg" />
+          <Heading color="primary.500">잠시만 기다려주세요...</Heading>
+        </HStack>
       </View>
-    </View>
+    </NativeBaseProvider>
   );
 };
 
-export default Loading;
+export default CommonLoading;
